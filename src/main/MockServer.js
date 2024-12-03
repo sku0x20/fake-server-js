3a3d3bb672b54576d0e67e4b8716f831235eda4d
siddhant <siddhantkumarupmanyu@gmail.com>
2024-12-03 16:15:01 +0530

    add default response

--- src/main/MockServer.js
+++ src/main/MockServer.js
@@ -1,15 +1,18 @@
 export default class MockServer {
 
     #receivedRequests = []
+    #defaultResponse = null
 
-    constructor() {
+    constructor(defaultResponse) {
+        this.#defaultResponse = defaultResponse
     }
 
     allVerified() {
         return this.#receivedRequests.length === 0
     }
 
-    handle(req) {
+    handle(req, res) {
         this.#receivedRequests.push(req)
+        this.#defaultResponse(req, res)
     }
 }
--- src/test/fake-server.test.js
+++ src/test/fake-server.test.js
@@ -5,7 +5,9 @@ import assert from 'node:assert/strict'
 let server = new MockServer()
 
 beforeEach(() => {
-    server = new MockServer()
+    server = new MockServer((req, res) => {
+        res.status = 404
+    })
 })
 
 test("noUnverifiedRequestsByDefault", () => {
@@ -17,6 +19,30 @@ test("unverifiedRequests", () => {
         method: 'GET',
         url: "/some-url"
     }
-    server.handle(req)
+    server.handle(req, {})
     assert.equal(server.allVerified(), false)
 })
+
+test("defaultResponse", () => {
+    const req = {}
+    const res = {status: undefined}
+    server.handle(req, res)
+    assert.equal(res.status, 404)
+})
+
+// test("respondWith", () => {
+//     const req = {
+//         method: 'GET',
+//         url: "/some-url"
+//     }
+//     const res = {
+//         req: req,
+//         status: 404
+//     }
+//
+//     server.respondWith(async (req, res) => {
+//         res.end("Responded")
+//     })
+//
+//     server.handle()
+// })
